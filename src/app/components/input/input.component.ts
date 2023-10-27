10fd86d954bb250ad515e78045214a48bd7a8a37
Jana Jankovic <jana.jankovic@nomnio.com>
2023-10-27 13:30:23 +0200

    inputs

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
 export class InputComponent {
-
+  @Input() inputType = 'text';
+  @Input() placeholder: string = '';
+  @Input() label: string = '';
+  @Input() startIcons = [];
+  @Input() endIcons = [];
+  @Input() wrapperClass = '';
+  @Input() inputClass = '';
 }
